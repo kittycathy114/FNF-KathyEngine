dbd71ebc20c2372d3ee1d3829836b48ec3137c1f
kittycathy233 <2286431462@qq.com>
2026-01-26 19:53:01 +0800

    AI在修改制谱器的同时把这个也给改了，目前不知道其作用

--- source/objects/Character.hx
+++ source/objects/Character.hx
@@ -288,9 +288,9 @@ class Character extends FlxSprite
 		}
 
 		if (getAnimationName().startsWith('sing')) holdTimer += elapsed;
-		else if(isPlayer) holdTimer = 0;
+		else holdTimer = 0;
 
-		if (!isPlayer && holdTimer >= Conductor.stepCrochet * (0.0011 #if FLX_PITCH / (FlxG.sound.music != null ? FlxG.sound.music.pitch : 1) #end) * singDuration)
+		if (holdTimer >= Conductor.stepCrochet * (0.0011 #if FLX_PITCH / (FlxG.sound.music != null ? FlxG.sound.music.pitch : 1) #end) * singDuration)
 		{
 			dance();
 			holdTimer = 0;
